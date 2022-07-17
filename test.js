ac8646a184b4675e08027dddbaf5ec5e41a301cd
zelwake <michalhladik2@seznam.cz>
2022-07-17 12:45:30 +0200

    exercise

---
+++ test.js
@@ -0,0 +1,20 @@
+let age = 2022 - 1993
+let ww2 = 1945 - 1939
+let tesla = 76 * 0.015
+let secondsOfDay = 24 * 60 * 60
+let hungarians = 9712887 / 7961175637 * 100
+let bigger = 9597000 / 78871
+let participants = 654 % 7
+
+let names = ["Míša", "Piškot", "Honzík", "Filip", "Geralt", "Kylie"]
+console.log(names[2].length > 5)
+names.push("Josiah")
+names[0] = "Michal"
+names.length > 4
+
+
+let colors = []
+let objectColors = {
+    colors: ["blue", "Yellow", "red", "cyan", "orange"],
+    hasManyColors: objectColors["colors.length" > 3],
+};
